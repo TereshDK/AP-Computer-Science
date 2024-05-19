b8ce3a5d3b52afd21f7ffc8e71f2992b54e06c0e
Teresh <101990647+GrozmanGit@users.noreply.github.com>
2024-05-19 16:37:18 -0500

    Fixed Indentation and Converted to .java

--- Unit 3 /Dog /Dog.java
+++ Unit 3 /Dog /Dog.java
@@ -1,1 +1,52 @@
+Dog
 
+```
+public class Dog
+{
+  private int age;
+  private String name;
+	
+  public Dog( )  
+  {
+    name = "spot"; 
+    age = 1;
+  }
+ 	
+  public Dog( String n )  
+  {
+    age = 1; 
+    name = n; 
+  }
+  
+  public Dog( String n, int a )  
+  {
+    this.name = n; 
+    this.age = a; 
+  }
+  
+  public void increaseAgeByOne()
+  {
+    age++;		  
+  }
+
+  public int getDogYears( int peopleYears )
+  {
+    return age * peopleYears; 
+  }
+
+  public int getAge()    
+  {
+    return age;
+  }
+
+  public String getName()    
+  {
+    return name; 
+  }
+	
+  public String toString()    
+  {
+    return "Dog: " + name + ", Age: " + age; 
+  }	
+} 
+```

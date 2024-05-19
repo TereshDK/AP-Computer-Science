e0d30f66b866f254eb224f3748f48ca13b94ad87
Teresh <101990647+GrozmanGit@users.noreply.github.com>
2024-05-19 16:59:48 -0500

    Convert to .java

--- Unit 8 /Array Funhouse 1 /ArrayFunHouse.java
+++ Unit 8 /Array Funhouse 1 /ArrayFunHouse.java
@@ -4,13 +4,7 @@ Array Funhouse
 import java.util.Arrays;
 public class ArrayFunHouse
 {
-	//instance variables and constructors could be used, but are not really needed for this lab
-   
-  //look at the instructions for sample output for each method you need to write
-   
-	//getSum() will return the sum of the numbers from start to stop inclusive
-  //Use a for loop to traverse the array (you can use an indexed loop or a for each loop)
-	public static int getSum(int[] numArray, int start, int stop)
+	public static int getTotal(int[] numArray, int start, int stop)
 	{
 		int total = 0; 
 		
@@ -22,7 +16,6 @@ public class ArrayFunHouse
 		return total; 
 	}
 
-	//getCount() will return number of times val is present
 	public static int getCount(int[] numArray, int val)
 	{
 		int count = 0;

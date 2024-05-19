8408d06cb926b4ca27a033e9676c329fa3e212b1
Teresh <101990647+GrozmanGit@users.noreply.github.com>
2024-05-19 17:01:20 -0500

    Convert to .java

--- Unit 8 /Array Funhouse 2 /Main.java
+++ Unit 8 /Array Funhouse 2 /Main.java
@@ -7,10 +7,10 @@ public class Main
 {
 	public static void main( String args[] )
 	{
-		int[] one = {1,2,3,4,5,6,7,8,9,10};
-		int[] two = {1,2,3,9,11,20,30};
-		int[] three = {9,8,7,6,5,4,3,2,1-1,-2};
-		int[] four = {3,6,9,12,15,18,21,23,19,17,15,13,11,10,9,6,3,2,1,0};
+		int[] one = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		int[] two = {1, 2, 3, 9, 11, 20, 30};
+		int[] three = {9, 8, 7, 6, 5, 4, 3, 2, 1-1, -2};
+		int[] four = {3, 6, 9, 12, 15, 18, 21, 23, 19, 17, 15, 13, 11, 10, 9, 6, 3, 2, 1, 0};
 
 
 		System.out.println(Arrays.toString(one));

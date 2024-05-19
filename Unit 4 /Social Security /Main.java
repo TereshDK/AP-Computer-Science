c83a29e36f52e3dbe0e21113e5662afb80629a01
Teresh <101990647+GrozmanGit@users.noreply.github.com>
2024-05-19 16:47:02 -0500

    Convert to .java

--- Unit 4 /Social Security /Main.java
+++ Unit 4 /Social Security /Main.java
@@ -1,1 +1,35 @@
+Main 
 
+```
+public class Main
+{
+  public static void main( String args[] )
+  {
+    Social ss = new Social(""); 
+       
+    System.out.println("\n");
+    ss.chopAndAdd("456-56-2345");
+    System.out.println(ss);
+    
+    System.out.println("\n");
+    ss.chopAndAdd("121-12-1200");  
+    System.out.println(ss);
+
+    System.out.println("\n");
+    ss.chopAndAdd("102-2-1212");
+    System.out.println(ss);
+
+    System.out.println("\n");
+    ss.chopAndAdd("000-00-0000");
+    System.out.println(ss);
+
+    System.out.println("\n");
+    ss.chopAndAdd("000-00-000");
+    System.out.println(ss);
+    
+    System.out.println("\n");
+    ss.chopAndAdd("175-99 5656");
+    System.out.println(ss);
+  }
+}
+```

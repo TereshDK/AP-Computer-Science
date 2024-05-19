4ac533247e0363e3f796ae2cecac1bcd715ef861
Teresh <101990647+GrozmanGit@users.noreply.github.com>
2024-05-19 16:59:24 -0500

    Convert to .java

--- Unit 8 /Array Funhouse 1 /Main.java
+++ Unit 8 /Array Funhouse 1 /Main.java
@@ -9,9 +9,9 @@ public class Main
 	{
 		int[] one = {4,10,0,1,7,6,5,3,2,9};
 
-		System.out.println(Arrays.toString(one));//prints the array
-		System.out.println("sum of spots 3-6  =  " + ArrayFunHouse.getSum(one,3,6));
-		System.out.println("sum of spots 2-9  =  " + ArrayFunHouse.getSum(one,2,9));
+		System.out.println(Arrays.toString(one));
+		System.out.println("total of spots 3-6  =  " + ArrayFunHouse.getTotal(one,3,6));
+		System.out.println("total of spots 2-9  =  " + ArrayFunHouse.getTotal(one,2,9));
 		System.out.println("# of 4s  =  " + ArrayFunHouse.getCount(one,4));
 		System.out.println("# of 7s  =  " + ArrayFunHouse.getCount(one,7));
 		System.out.println("before removing all 7s " + Arrays.toString(one));
@@ -24,8 +24,8 @@ public class Main
     int[] two = {4,2,3,4,6,7,8,9,0,10,0,1,7,6,5,3,2,9,9,8,7};
      
     System.out.println(Arrays.toString(two));//prints the array
-		System.out.println("sum of spots 20-21  =  " + ArrayFunHouse.getSum(two,20,21));
-		System.out.println("sum of spots 8-11  =  " + ArrayFunHouse.getSum(two,8,11));
+		System.out.println("total of spots 20-21  =  " + ArrayFunHouse.getTotal(two,20,21));
+		System.out.println("total of spots 8-11  =  " + ArrayFunHouse.getTotal(two,8,11));
 		System.out.println("# of 2s  =  " + ArrayFunHouse.getCount(two,2));
 		System.out.println("# of 9s  =  " + ArrayFunHouse.getCount(two,9));
 		System.out.println("before removing all 9s " + Arrays.toString(two));

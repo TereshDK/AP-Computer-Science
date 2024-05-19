6142ff61e441ac8f60789645268693deddfd2e5a
Teresh <101990647+GrozmanGit@users.noreply.github.com>
2024-05-19 17:01:51 -0500

    Convert to .java

--- Unit 8 /Array Funhouse 2 /ArrayFunHouse.java
+++ Unit 8 /Array Funhouse 2 /ArrayFunHouse.java
@@ -6,10 +6,6 @@ import java.lang.Math;
 
 public class ArrayFunHouseTwo
 {
-	//goingUp() will return true if all numbers
-	//in numArray are in increasing order
-	//[1,2,6,9,23] returns true
-	//[9, 11, 13, 8]  returns false
 	public static boolean goingUp(int[] nArray)
 	{
 		int count = 0; 
@@ -27,10 +23,6 @@ public class ArrayFunHouseTwo
 		return false; 
 	}
 
-	//goingDown() will return true if all numbers
-	//in numArray are in decreasing order
-	//[31,12,6,2,1] returns true
-	//[31, 20, 10, 15, 9] returns false
 	public static boolean goingDown(int[] nArray)
 	{
 	int count = 0; 
@@ -47,10 +39,7 @@ public class ArrayFunHouseTwo
 		}
 		return false; 
 	}
-
-	//getValuesBiggerThanX will return an array that contains
-	//count number of values that are larger than parameter x
-	//[1,2,3,4,5,6,7,8,9,10,11,6],3,5  would return [6,7,8]
+    
 	public static int[] getCountValuesBiggerThanX(int[] nArray, int count, int x)
 	{
 	int[] sect = new int[count]; 

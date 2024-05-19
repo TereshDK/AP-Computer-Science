92c1831172907e9733120e9f1567127f44e4250b
Teresh <101990647+GrozmanGit@users.noreply.github.com>
2024-05-19 16:57:41 -0500

    Convert to .java

--- Unit 7 /Digit Averager /Main.java
+++ Unit 7 /Digit Averager /Main.java
@@ -5,7 +5,6 @@ public class Main
 {
 	public static void main( String args[] )
 	{
-		//add test cases 
     digitAverager n = new digitAverager(); 
     
     System.out.println(n.sumDigits(234)); 

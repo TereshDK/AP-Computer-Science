56847db6be39958e29bc400b08628beff1872b82
Teresh <101990647+GrozmanGit@users.noreply.github.com>
2024-05-19 17:07:46 -0500

    Convert to .java

--- Unit 9 /Words /Word.java
+++ Unit 9 /Words /Word.java
@@ -1,34 +1,26 @@
 Word
 
 ```
-/***PART 1***/
-/*This part of the lab is designed to review classes, objects, and methods*/
-/*It does not require ArrayLists*/
 public class Word
 {
 	private String word;
    private static String vowels = "AEIOUaeiou";   
 
-	/*complete the default constructor method*/
    public Word()
 	{
 		word = ""; 
 	}
-
-   /*complete the constructor method*/
+	
 	public Word(String wrd)
 	{
 		word = wrd; 
 	}
 
-   /*setWord should initialize the appropriate instance variable*/
 	public void setWord(String wrd)
 	{
 		word = wrd; 
 	}
-	
-   /*getNumVowels returns the number of vowels in the word*/
-   /*HINT: Check to see if each char in the word is contained with in the vowels string.*/
+
 	public int getVowels()
 	{
 		int count = 0;
@@ -46,13 +38,11 @@ public class Word
 		return count;
 	}
 	
-	/*getLength returns the length of the word*/
    public int getLength()
 	{
 		return word.length(); 
 	}
 
-	/*toString will return the word*/
    public String toString()
 	{
 	   return ""+word;

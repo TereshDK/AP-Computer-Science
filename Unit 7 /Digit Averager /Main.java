5f44767d4db988ee59875924340c5565cea570dd
Teresh <101990647+GrozmanGit@users.noreply.github.com>
2024-05-19 16:58:00 -0500

    Convert to .java

--- Unit 7 /Digit Averager /Main.java
+++ Unit 7 /Digit Averager /Main.java
@@ -7,19 +7,19 @@ public class Main
 	{
     digitAverager n = new digitAverager(); 
     
-    System.out.println(n.sumDigits(234)); 
+    System.out.println(n.totalDigits(234)); 
 
-    System.out.println(n.sumDigits(10000)); 
+    System.out.println(n.totalDigits(10000)); 
 
-    System.out.println(n.sumDigits(111)); 
+    System.out.println(n.totalDigits(111)); 
        
-    System.out.println(n.sumDigits(9005)); 
+    System.out.println(n.totalDigits(9005)); 
       
-    System.out.println(n.sumDigits(84645)); 
+    System.out.println(n.totalDigits(84645)); 
     
-    System.out.println(n.sumDigits(8547)); 
+    System.out.println(n.totalDigits(8547)); 
     
-    System.out.println(n.sumDigits(123456789)); 
+    System.out.println(n.totalDigits(123456789)); 
 	}
 }
 ```
